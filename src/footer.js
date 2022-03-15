21719985d12d53d8e94de7e05e826933cf9644a2
c4zuko <101098340+c4zuko@users.noreply.github.com>
2022-03-15 19:03:12 +0530

    Add files via upload

---
+++ src/footer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    /*<footer className="footer">
+      All rights reserved.
+    </footer>*/
+    <footer > <small>&copy; Copyright 2018, Example Corporation</small> </footer> 
+    
+  )
+}
+/*
+  <div className="ui serach"  className="tables">
+  <div className='ui serach icon'>
+    <input type="text" placeholder="search bar" className="prompt"/>
+    <i className="search icon"></i> 
+  </div>
+</div>*/
